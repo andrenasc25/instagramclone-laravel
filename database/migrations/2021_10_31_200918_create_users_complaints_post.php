dbfd769b4fc62c9ee45209ef6e1f1141686c8ab5
andren25 <guerreiroandre25@gmail.com>
2021-10-31 17:13:00 -0300

    create_users_complaints_post added

---
+++ database/migrations/2021_10_31_200918_create_users_complaints_post.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersComplaintsPost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_complaints_post', function (Blueprint $table) {
+            $table->id();
+
+            //Foreign keys
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('user_post_id');
+            $table->foreign('user_post_id')->references('id')->on('users_posts');
+
+            //Normal column
+            $table->string('complaint');
+
+            $table->timestamp('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_complaints_post');
+    }
+}
