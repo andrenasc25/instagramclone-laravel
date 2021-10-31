b970562ec3a56b442653addf9c65c9d3e3d4c922
andren25 <guerreiroandre25@gmail.com>
2021-10-31 16:48:58 -0300

    create_users_posts_table added

---
+++ database/migrations/2021_10_31_194459_create_users_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_posts', function (Blueprint $table) {
+            $table->id();
+
+            //Foreign keys
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            //Normal columns
+            $table->longText('users_post_text');
+            $table->timestamp('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_posts');
+    }
+}
