5942133b3b2f5becb2d816e61c977dcef9c73bed
andren25 <guerreiroandre25@gmail.com>
2021-10-31 16:53:07 -0300

    create_search_hitories_table added

---
+++ database/migrations/2021_10_31_195133_create_search_histories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSearchHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('search_histories', function (Blueprint $table) {
+            $table->id();
+            $table->longText('search_history_keyword');
+
+            //Foreign keys
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            //Normal column
+            $table->timestamp('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('search_histories');
+    }
+}
