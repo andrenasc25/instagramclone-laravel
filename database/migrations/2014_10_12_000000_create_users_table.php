35a56eb376053a2005be068d64d05e6f24bdc610
andren25 <guerreiroandre25@gmail.com>
2021-10-31 16:27:05 -0300

    create_users_table added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            
+            //Columns that reflect the table's name
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->Integer('user_address_cep')->nullable();
+            $table->longText('user_address_street')->nullable();
+            $table->longText('user_address_neighbourhood')->nullable();
+            $table->Integer('user_address_number')->nullable();
+            $table->longText('user_address_complement')->nullable();
+            $table->bigInteger('user_phone')->nullable();
+            $table->longText('user_profile_image_url')->nullable();
+            $table->Integer('user_access_level')->default('1');
+            $table->Integer('user_status')->nullable();
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('user_remember_token_expires_at')->nullable();
+            $table->timestamp('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
