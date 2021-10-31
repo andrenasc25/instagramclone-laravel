ce9d88ee457b52a78c1dbd5521c76422ac4f8366
andren25 <guerreiroandre25@gmail.com>
2021-10-31 17:48:42 -0300

    create_users_like_users_comments_posts added

---
+++ database/migrations/2021_10_31_204706_create_users_like_users_comments_posts.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersLikeUsersCommentsPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_like_users_comments_posts', function (Blueprint $table) {
+            $table->id();
+
+            //Foreign keys
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('user_comment_post_id');
+            $table->foreign('user_comment_post_id')->references('id')->on('users_comments_posts');
+
+            $table->timestamp('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_like_users_comments_posts');
+    }
+}
