a49275276d76e427f85f2f5c2c32812d20df7c88
andren25 <guerreiroandre25@gmail.com>
2021-10-31 18:52:06 -0300

    updated renamed migrations

--- database/migrations/2021_10_31_195900_create_users_comments_posts_table.php
+++ database/migrations/2021_10_31_195900_create_users_comments_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersCommentsPosts extends Migration
+class CreateUsersCommentsPostsTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2021_10_31_201801_create_users_likes_posts_table.php
+++ database/migrations/2021_10_31_201801_create_users_likes_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersLikesPosts extends Migration
+class CreateUsersLikesPostsTable extends Migration
 {
     /**
      * Run the migrations.
