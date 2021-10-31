a20de3972ab6a34cc6ddd6f368870bedc3f47b3e
andren25 <guerreiroandre25@gmail.com>
2021-10-31 17:17:16 -0300

    create_users_in_chats added

---
+++ database/migrations/2021_10_31_201409_create_users_in_chats_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersInChats extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_in_chats', function (Blueprint $table) {
+            $table->id();
+
+            //Foreign keys
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('user_chat_message_id');
+            $table->foreign('user_chat_message_id')->references('id')->on('users_chats_messages');
+
+            $table->timestamp('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_in_chats');
+    }
+}
