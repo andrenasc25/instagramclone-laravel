838b4fe9c130f59255907533a39ed4ab1de461e4
andren25 <guerreiroandre25@gmail.com>
2021-10-31 17:04:49 -0300

    create_users_comments_stories added

---
+++ database/migrations/2021_10_31_200308_create_users_comments_stories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersCommentsStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_comments_stories', function (Blueprint $table) {
+            $table->id();
+
+            //Foreign keys
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('user_story_id');
+            $table->foreign('user_story_id')->references('id')->on('users_stories');
+
+            $table->timestamp('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_comments_stories');
+    }
+}
