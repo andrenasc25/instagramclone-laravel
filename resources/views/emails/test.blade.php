bd662b4856628f2749838cbdcdbfeb1d7fc52505
andren25 <guerreiroandre25@gmail.com>
2021-10-31 21:41:02 -0300

    emails.test blade added

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <h2>Test Email</h2>
+    <a href="{{$test_message}}">{{ $test_message }}</a>
+  </body>
+</html>
