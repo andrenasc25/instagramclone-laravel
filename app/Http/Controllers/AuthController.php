d09f8d3075d1e52f7ec00d25e2b3ccb8d6816159
andren25 <guerreiroandre25@gmail.com>
2021-10-31 21:21:47 -0300

    register method added to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Mail\MailSend;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 class AuthController extends Controller
 {
-    
+    public function register(Request $request)
+    {
+        //Get data from the user
+        $name = $request->name;
+        $email = $request->email;
+        $password = Hash::make($request->password);
+
+        //Insert new user in users table
+        User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        //If the data is inserted successfully, create a temporary link to be sent via email
+        $url = URL::temporarySignedRoute(
+            'activateLink', now()->addMinutes(30), ['mail' => $mail]
+        );
+
+        //Send the created link to the user's email
+        $mailSend = new MailSend();
+        $mailSend->send($email, $url, 'Confirmação de email');
+
+        return 'Email de confirmação enviado para o email: ' . $email;
+    }
 }
