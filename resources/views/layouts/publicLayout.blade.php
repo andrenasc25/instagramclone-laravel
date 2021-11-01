33c37b247afbcc876e37e5462d8a50c6884dd746
andren25 <guerreiroandre25@gmail.com>
2021-10-31 21:27:43 -0300

    publicLayout updated

--- resources/views/layouts/publicLayout.blade.php
+++ resources/views/layouts/publicLayout.blade.php
@@ -6,16 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/chat.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/explore.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/config.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
     <link href="{{ asset('css/signup.css') }}" rel="stylesheet">
     <script src="{{asset('js/vendor/axios.min.js')}}"></script>
-    <script src="{{asset('js/setCookie.js')}}"></script>
     <title>Instagram Clone</title>
 </head>
 <body>
