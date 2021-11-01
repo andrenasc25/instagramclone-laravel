ce15dcefb9b747ede49f781a72add458b0612bbc
andren25 <guerreiroandre25@gmail.com>
2021-10-31 21:33:29 -0300

    AuthController updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Mail\MailSend;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\URL;
 
 class AuthController extends Controller
@@ -26,7 +27,7 @@ class AuthController extends Controller
 
         //If the data is inserted successfully, create a temporary link to be sent via email
         $url = URL::temporarySignedRoute(
-            'activateLink', now()->addMinutes(30), ['mail' => $mail]
+            'activateLink', now()->addMinutes(30), ['mail' => $email]
         );
 
         //Send the created link to the user's email
